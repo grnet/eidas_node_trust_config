a8fe97d2c893eddd5535f7ba1fa4fec1406d8c5b
Zenon Mousmoulas <zmousm@noc.grnet.gr>
2024-06-15 20:09:46 +0300

    Switch to SHA256 cert fingerprints
    For now just duplicate more code from xmlsec.crypto._cert_fingerprint

--- eidas_node_trust_config/utils.py
+++ eidas_node_trust_config/utils.py
@@ -1,6 +1,8 @@
 import datetime
+from binascii import hexlify
 from xmlsec.crypto import _cert_fingerprint as xmlsec_cert_fingerprint
 from cryptography.hazmat.primitives.serialization import Encoding as CryptoSerializationEncoding
+from cryptography.hazmat.primitives.hashes import SHA256
 
 def is_cert_expired(cert):
     if hasattr(cert, 'not_valid_after_utc'):
@@ -12,8 +14,7 @@ def update_fp_pem_mapping(certificates, cert, filter_expired=True):
     fp, cert = xmlsec_cert_fingerprint(cert)
     if filter_expired and is_cert_expired(cert):
         return
-    fp = fp.replace(':', '')
-    # if hasattr(cert, 'public_bytes'):
+    fp = hexlify(cert.fingerprint(SHA256())).lower().decode('ascii')
     cert = cert.public_bytes(CryptoSerializationEncoding.PEM).decode('ascii')
     certificates.update({fp: cert})
 
